112c84ee5c668aa8b2cae195c4f0bf624d239e0f
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-09-28 15:47:35 +0100

    add rest controller

--- src/main/java/com/healthcare/patient_management/controller/PatientController.java
+++ src/main/java/com/healthcare/patient_management/controller/PatientController.java
@@ -1,4 +1,41 @@
 package com.healthcare.patient_management.controller;
 
+import com.healthcare.patient_management.dto.ApiResponse;
+import com.healthcare.patient_management.dto.NewPatientResponse;
+import com.healthcare.patient_management.dto.NewUserRequestDto;
+import com.healthcare.patient_management.dto.PatientRequest;
+import com.healthcare.patient_management.service.implementation.PatientServiceImpl;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patient-management")
 public class PatientController {
+
+    private final PatientServiceImpl patientService;
+
+    @Autowired
+    public PatientController(PatientServiceImpl patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping("new")
+    public ResponseEntity<ApiResponse<NewPatientResponse>> newPatient(@RequestBody PatientRequest newPatient){
+        NewPatientResponse patient = patientService.newPatient(newPatient);
+
+        ApiResponse<NewPatientResponse> response = new ApiResponse<>(
+                true,
+                "Patient has been added successfully",
+                patient
+        );
+
+        return ResponseEntity
+                .status(200)
+                .body(response);
+    }
 }
